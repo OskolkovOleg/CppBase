2c1b84fa2ea38070336b47a33b9e711486266e33
Oleg Oskolkov <oskolkov.web@gmail.com>
2023-11-11 08:58:46 +0300

    ваыа

---
+++ 1.Syntax/1.3.Operators_and_Expressions/1.2.1.Arithmetic/1.2.1.1.Arithmetic.cpp
@@ -0,0 +1,70 @@
+﻿////////Арифметические опреации
+//
+//#include <iostream>
+//
+//int main() {
+//	/*float a = 5.8, b = 7.6;
+//	double c = a - b;
+//
+//	int d = (int)(a - b);
+//	int d2 = (int)a + b;
+//
+//	std::cout << c << " " << d << " " << d2 << std::endl;*/
+//
+//	/*double a, b;
+//	std::cin >> a >> b;
+//
+//	double p = 2 * (a + b);
+//
+//	std::cout << "p = " << p;*/
+//
+//	int a = 8;
+//	int b = 3;
+//
+//	double c = a / b;
+//	double c2 = a / (double)b;
+//	double c3 = (double)a / b;
+//	// То есть если хотя бы один из сножителей дробный тогда 
+//	// ответ дробный иначе отбрасывание дробной части
+//	std::cout << c << std::endl << c2 << std::endl << c3;
+//
+//
+//	/*int ost = 10 % 3;
+//
+//	std::cout << ost;
+//
+//	int var = 0;
+//	var++;
+//	std::cout << var << std::endl;
+//
+//	float arg = 5;
+//	arg--;
+//	--arg;
+//	std::cout << arg << std::endl;*/
+//
+//	//int a, b, c = 10, d = 10;
+//	//a = c++; // Сначала присаивается а потом с увеличвается
+//	//b = ++d; // Сначала увеличивается
+//	//
+//	//std::cout << a << ' ' << b << ' ' << c << ' ' << d;
+//
+//	//int a = 0, b = 0, c = 0, d = 0;
+//	//a += 5;
+//	//b -= 3;
+//
+//	//c *= 2;
+//	//d /= 4;
+//
+//	/*int h, d;
+//
+//	std::cin >> h >> d;
+//	float s = 0.5 * h * d;
+//	std::cout << s;
+//
+//	double s;
+//	s = 1 / 2.0 - 1 / 4.0 + 1 / 5.0 - 1 / 7.0 + 1 / 10.0;
+//	std::cout << s << std::endl;*/
+//
+//
+//	return 0;
+//}
